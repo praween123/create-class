2a63aee3f355749063a30000bddcc9535e7c1b5b
praween123 <praweendissanayake09@gmail.com>
2024-02-04 13:23:09 +0530

    class

--- demo3.java
+++ demo3.java
@@ -2,9 +2,9 @@
 public class demo3{
 	public static void main(String args[]){
 		
-		System.out.print("your ag");
-
 		System.out.print("your age");
+
+		System.out.print("your address");
 
 		System.out.print("your Name");
 		
